c8d5c5f8530306b3f986b1c2a34af6a1a172db64
Rizuraa <mfmaulana08@gmail.com>
2024-09-06 14:37:06 +0700

    finish app

---
+++ components/OAuth.tsx
@@ -0,0 +1,52 @@
+import { Alert, Image, Text, View } from "react-native";
+import CustomButton from "./CustomButton";
+import { icons } from "@/constants";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useCallback } from "react";
+import { googleOAuth } from "@/lib/auth";
+import { router } from "expo-router";
+
+const OAuth = () => {
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleGoogleSignIn = useCallback(async () => {
+    try {
+      const result = await googleOAuth(startOAuthFlow);
+
+      if (result.code === "session_exist" || result.code === "success") {
+        Alert.alert("succes", "session exist. Directing to Home");
+        router.push("/(root)/(tabs)/home");
+      }
+
+      // Alert.alert(result.success ? "succes" : "error", result.message);
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
+  return (
+    <View>
+      <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
+        <View className="flex-1 h-[1px] bg-general-100" />
+        <Text className="text-lg">Or</Text>
+        <View className="flex-1 h-[1px] bg-general-100" />
+      </View>
+      <CustomButton
+        title="Login with Google"
+        className="mt-5 w-full shadow-none"
+        IconLeft={() => (
+          <Image
+            source={icons.google}
+            resizeMode="contain"
+            className="w-5 h-5 mx-2"
+          />
+        )}
+        bgVariant="outline"
+        textVariant="primary"
+        onPress={handleGoogleSignIn}
+      />
+    </View>
+  );
+};
+
+export default OAuth;
